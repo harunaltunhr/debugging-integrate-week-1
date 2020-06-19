8ef2f9898f3d257160df64433605e3bea54f5a27
harunaltunhr <altunhr@gmail.com>
2020-06-19 22:34:05 +0200

    Completed exercises

--- scripts/4-magic-number.js
+++ scripts/4-magic-number.js
@@ -19,14 +19,14 @@ function magicNumberHandler() {
   const message = `your magic number is: " + magicNumber + "`;
 
   // alert result for the user
-  console.alert(mesage);
+  console.alert(message);
 
   // log action for the developer
   console.log('\n--- magic number ---');
   console.log('input1:', typeof input1, '\n', input1);
   console.log('favenumber:', typeof favenumber, '\n', favenumber);
   console.log('input2:', typeof input2, '\n', input2);
-  console.log('birthDate:', typeof birthDate '\n', birthDate);
+  console.log('birthDate:', typeof birthDate, '\n', birthDate);
   console.log('message:', typeof message, '\n', message);
 
 }
